48534d51f9c6c7b1545d35f549e28dd3da077719
carlossantos <carlossantos@pop-os.localdomain>
2020-11-24 12:06:49 -0400

    Insertion Sort

---
+++ 2-insertionsort.cpp
@@ -0,0 +1,43 @@
+#include <stdio.h>
+// #include <display.h>
+#include <iostream>
+
+using namespace std;
+void display(int *array, int size) {
+  for(int i = 0; i < size; i++) 
+    cout << array[i] << " ";
+  cout << endl;
+}
+ 
+void insertionSort(int *array, int size) {
+  int key, j;
+  for(int i = 1; i < size; i++){
+    key = array[i];   //pega valor
+    j = i;
+    while (j > 0 && array[j -1] > key)
+    {
+      array[j] = array[j-1];
+      j--;
+    }
+    array[j] = key;    //insira no lugar certo
+  }
+}
+
+int main() {
+  int n;
+  cout << "Enter the number of elements: ";
+  cin >> n;
+  int arr[n];   //crie uma matriz com determinado número de elementos
+  cout << "Enter elements: " << endl;
+  for (int i = 0; i < n; i++)
+  {
+    cin >> arr[i];
+  }
+  cout << "Array before Sorting: ";
+  display(arr, n);
+  insertionSort(arr, n);
+  cout << "Array after Sorting: ";
+  display(arr, n);
+}
+
+
